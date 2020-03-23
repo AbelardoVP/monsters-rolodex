a4ed249911e5e100e3b97da0853e3664ce06b1b8
Abelardo Valdez <abelardo@softwaredevtools.com>
2020-03-22 20:35:33 -0600

    Updates

---
+++ precache-manifest.22f72f664ce5c7f7eaf7b16c496234fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b6fe2edd903bd5e62b67b15a7a63ef6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6c99f41d559eb53567a5",
+    "url": "/monsters-rolodex/static/css/main.54a9d3ad.chunk.css"
+  },
+  {
+    "revision": "76e1e33d0dc86b7a1eb3",
+    "url": "/monsters-rolodex/static/js/2.2e94f083.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.2e94f083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c99f41d559eb53567a5",
+    "url": "/monsters-rolodex/static/js/main.fa6821ac.chunk.js"
+  },
+  {
+    "revision": "f9890c86000462b21f2c",
+    "url": "/monsters-rolodex/static/js/runtime-main.a00addf9.js"
+  }
+]);
